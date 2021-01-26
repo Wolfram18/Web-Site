c019c384d4d497009d20078a7f1025806d783065
Wolfram18 <56358902+Wolfram18@users.noreply.github.com>
2021-01-26 23:28:22 +0300

    .gitignore is now working

--- AntiSite/anti/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from anti.models import Law
-
-
-class LawAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Law, LawAdmin)
--- AntiSite/anti/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Law(models.Model):
-    title = models.CharField(max_length=20)
-    text = models.TextField()
-    canon = models.TextField(blank=True)
